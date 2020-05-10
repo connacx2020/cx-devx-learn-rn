e113fedd436aa6fa07c6ea3b041a63648cc956ad
Hozon <40877374+Horizon19911@users.noreply.github.com>
2020-05-10 10:30:39 +0630

    Update Search.tsx

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import {SearchStackNavProps} from '../../ultis/ParamLists/SearchParamList';
+import { SearchStackNavProps } from '../../ultis/ParamLists/SearchParamList';
 
 
 function CxDevxSearch({ navigation }: SearchStackNavProps<"Search">) {
